a20aece10616fb34df2bdf55605c9b5f0d931246
praitayini <praitayini@gmail.com>
2022-02-15 17:59:02 -0600

    plots for spie talk

---
+++ stat_src/plot_spie_talk.py
@@ -0,0 +1,55 @@
+import numpy as np
+import nibabel as nib
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+true_md = nib.load('/home/local/VANDERBILT/kanakap/gradtensor_data/10_29_2019_human_repositioned/3tb/posA/OUTPUTS_future_fieldmap/p_3tb_posA_mask_md.nii').get_fdata()
+corpt_md = nib.load('/home/local/VANDERBILT/kanakap/gradtensor_data/10_29_2019_human_repositioned/3tb/posA/OUTPUTS_estimates_study/Lest_md.nii').get_fdata()
+ape_md = nib.load('/home/local/VANDERBILT/kanakap/gradtensor/src/md_ape_image.nii').get_fdata()
+
+plt.figure();
+slice_idx = 45
+slice = true_md[:,slice_idx,:]
+m = 0
+M = 0.03
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,1,1)
+plt.axis('off')
+cmap = plt.get_cmap('viridis')
+im = plt.imshow(np.abs(slice), vmin=m, vmax=M, cmap=cmap)
+plt.title('Intended FA')
+divider = make_axes_locatable(plt.gca())
+ax = divider.append_axes("right", size="5%", pad=0.05)
+a = plt.colorbar(im, cax=ax)
+
+slice = corpt_md[:,slice_idx,:]
+m = 0
+M = 0.03
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,1,2)
+plt.axis('off')
+cmap = plt.get_cmap('viridis')
+im = plt.imshow(np.abs(slice), vmin=m, vmax=M, cmap=cmap)
+plt.title('Achieved FA')
+divider = make_axes_locatable(plt.gca())
+ax = divider.append_axes("right", size="5%", pad=0.05)
+a = plt.colorbar(im, cax=ax)
+
+slice = ape_md[:,slice_idx,:]
+m = slice.min()
+M = slice.max()
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,1,3)
+plt.axis('off')
+cmap = plt.get_cmap('viridis')
+im = plt.imshow(np.abs(slice), vmin=m, vmax=M, cmap=cmap)
+plt.title('Non-linear effects')
+divider = make_axes_locatable(plt.gca())
+ax = divider.append_axes("right", size="5%", pad=0.05)
+a = plt.colorbar(im, cax=ax)
+plt.show()
