776ad519da693271ad06b7eb2e6bb6c76a8466e4
praitayini <praitayini@gmail.com>
2022-03-18 11:04:12 -0500

    plot ape masivar

--- stat_src/ape_masiver.py
+++ stat_src/ape_masiver.py
@@ -33,7 +33,7 @@ corpt_rd = nib.load('/home/local/VANDERBILT/kanakap/gradtensor_data/10_29_2019_h
 sm_corr_rd = nib.load('/home/local/VANDERBILT/kanakap/gradtensor_data/10_29_2019_human_repositioned/3tb/posA/OUTPUTS_masiver/approx/Lest_corr_sm_rd.nii').get_fdata()
 bx_corr_rd = nib.load('/home/local/VANDERBILT/kanakap/gradtensor_data/10_29_2019_human_repositioned/3tb/posA/OUTPUTS_masiver/full/Lest_corr_bx_rd.nii').get_fdata()
 
-plt.figure();
+fig,fax=plt.subplots(3,5);
 slice_idx = 52
 mask = mask[slice_idx,:,:]
 mask = np.array(mask, dtype=bool)
@@ -49,11 +49,11 @@ slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
 plt.subplot(3,5,1)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution FA", fontdict = {'fontsize' : 15})
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution FA", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
 err_md = corpt_md - true_md
 pe_md = 100 * err_md / true_md
@@ -66,11 +66,11 @@ slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
 plt.subplot(3,5,2)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution MD", fontdict = {'fontsize' : 15})
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution MD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
 def angular_error(PEa, PEb, halfPi=True):
     # PEa = preprocessing.normalize(PEa, norm='l1', axis=1)
@@ -97,14 +97,14 @@ slice = np.nan_to_num(slice)
 plt.subplot(3,5,5)
 plt.axis('off')
 #cmap = plt.get_cmap('hot')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution PEV", fontdict = {'fontsize' : 15})
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution PEV", fontdict = {'fontsize' : 15})
 #plt.title('Angular change in \n Corruption and Ground Truth')
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
-a.ax.tick_params(labelsize=12)
-a.set_label('degrees', size = 12)
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+#a.ax.tick_params(labelsize=12)
+#a.set_label('degrees', size = 12)
 
 err_ad = corpt_ad - true_ad
 pe_ad = 100 * err_ad / true_ad
@@ -117,11 +117,11 @@ slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
 plt.subplot(3,5,3)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution AD", fontdict = {'fontsize' : 15})
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution AD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
 err_rd = corpt_rd - true_rd
 pe_rd = 100 * err_rd / true_rd
@@ -134,11 +134,11 @@ slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
 plt.subplot(3,5,4)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution RD", fontdict = {'fontsize' : 15})
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution RD", fontdict = {'fontsize' : 15})
 divider = make_axes_locatable(plt.gca())
 ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+a = fig.colorbar(im, cax=ax,shrink=0.6)
 
 err_fa = corpt_fa - true_fa
 pe_fa = 100 * err_fa / true_fa
@@ -151,15 +151,31 @@ slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
 plt.subplot(3,5,1)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution FA", fontdict = {'fontsize' : 15})
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Corrpution FA", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
 
+err_fa = bx_corr_fa - true_fa
+pe_fa = 100 * err_fa / true_fa
+ape_fa = np.abs(pe_fa)
+slice = ape_fa[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,6)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Emp. Correction FA", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
-err_md = corpt_md - true_md
+err_md = bx_corr_md - true_md
 pe_md = 100 * err_md / true_md
 ape_md = np.abs(pe_md)
 slice = ape_md[slice_idx,:,:]
@@ -168,16 +184,154 @@ m = 0
 M = 10
 slice = np.flip(np.rot90(slice,3))
 slice = np.nan_to_num(slice)
-plt.subplot(3,5,2)
+plt.subplot(3,5,7)
 plt.axis('off')
-im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'afmhot')
-plt.title("Corrpution MD", fontdict = {'fontsize' : 15})
-divider = make_axes_locatable(plt.gca())
-ax = divider.append_axes("right", size="5%", pad=0.05)
-a = plt.colorbar(im, cax=ax)
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Emp. Correction MD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
+pev_err = angular_error(bx_corr_pev, true_pev)
+slice = pev_err[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 5
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,10)
+plt.axis('off')
+#cmap = plt.get_cmap('hot')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Emp. Correction PEV", fontdict = {'fontsize' : 15})
+#plt.title('Angular change in \n Corruption and Ground Truth')
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+#a.ax.tick_params(labelsize=12)
+#a.set_label('degrees', size = 12)
 
+err_ad = bx_corr_ad - true_ad
+pe_ad = 100 * err_ad / true_ad
+ape_ad = np.abs(pe_ad)
+slice = ape_ad[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,8)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Emp. Correction AD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
 
+err_rd = sm_corr_rd - true_rd
+pe_rd = 100 * err_rd / true_rd
+ape_rd = np.abs(pe_rd)
+slice = ape_rd[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,9)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction RD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+
+err_fa = sm_corr_fa - true_fa
+pe_fa = 100 * err_fa / true_fa
+ape_fa = np.abs(pe_fa)
+slice = ape_fa[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,11)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction FA", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+
+err_md = sm_corr_md - true_md
+pe_md = 100 * err_md / true_md
+ape_md = np.abs(pe_md)
+slice = ape_md[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,12)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction MD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+
+pev_err = angular_error(sm_corr_pev, true_pev)
+slice = pev_err[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 5
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,15)
+plt.axis('off')
+#cmap = plt.get_cmap('hot')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction PEV", fontdict = {'fontsize' : 15})
+#plt.title('Angular change in \n Corruption and Ground Truth')
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+#a.ax.tick_params(labelsize=12)
+#a.set_label('degrees', size = 12)
+
+err_ad = sm_corr_ad - true_ad
+pe_ad = 100 * err_ad / true_ad
+ape_ad = np.abs(pe_ad)
+slice = ape_ad[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,13)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction AD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(, cax=ax)
+
+err_rd = sm_corr_rd - true_rd
+pe_rd = 100 * err_rd / true_rd
+ape_rd = np.abs(pe_rd)
+slice = ape_rd[slice_idx,:,:]
+slice = np.where(mask, slice, 0)
+m = 0
+M = 10
+slice = np.flip(np.rot90(slice,3))
+slice = np.nan_to_num(slice)
+plt.subplot(3,5,14)
+plt.axis('off')
+im = plt.imshow(slice, vmin=m, vmax=M, cmap= 'viridis')
+#plt.title("Approx. Correction RD", fontdict = {'fontsize' : 15})
+#divider = make_axes_locatable(plt.gca())
+#ax = divider.append_axes("right", size="5%", pad=0.05)
+#a = plt.colorbar(im, cax=ax)
+plt.subplots_adjust(hspace=0, wspace=0,bottom=0.39,left=0.026,right=0.96)
 plt.show()
 
 
