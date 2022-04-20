75b52a6ca9bdca15df1faab6132055a2adc3a70f
praitayini <praitayini@gmail.com>
2022-04-20 06:34:59 -0500

    update snr boxplot

--- stat_src/ape_masiver_boxplot.py
+++ stat_src/ape_masiver_boxplot.py
@@ -48,106 +48,45 @@ def plot_one_snr(corpt_fa,true_fa,snr):
     df3 = pd.DataFrame(ape_fa_csf).assign(Tissue='CSF')
     cdf = pd.concat([df1, df2, df3])
     cdf = cdf.assign(SNR = snr)
-    return cdf
+    median = cdf.groupby(['SNR', 'Tissue'])[0].median().values
+    idx = [1,2,0]
+    median_sort = median[idx]
+    return cdf, median
 
-cdf = plot_one_snr(corpt_fa,true_fa,'inf')
-cdf_ec = plot_one_snr(bx_corr_fa,true_fa,'inf ec')
+cdf,median = plot_one_snr(corpt_fa,true_fa,'inf')
+cdf_ec,median_ec = plot_one_snr(bx_corr_fa,true_fa,'inf ec')
+cdf_ac,median_ac = plot_one_snr(sm_corr_fa,true_fa,'inf ac')
 
-cdf100 = plot_one_snr(corpt_fa100,true_fa,'100')
-cdf30 = plot_one_snr(corpt_fa30,true_fa,'30')
-cdf10 = plot_one_snr(corpt_fa10,true_fa,'10')
+cdf100,median100 = plot_one_snr(corpt_fa100,true_fa,'100')
+cdf_ec100,median_ec100 = plot_one_snr(bx_corr_fa100,true_fa,'100 ec')
+cdf_ac100,median_ac100 = plot_one_snr(sm_corr_fa100,true_fa,'100 ac')
 
-all_cdf = pd.concat([cdf,cdf_ec, cdf100, cdf30, cdf10])
+cdf30,median30 = plot_one_snr(corpt_fa30,true_fa,'30')
+cdf_ec30,median_ec30 = plot_one_snr(bx_corr_fa30,true_fa,'30 ec')
+cdf_ac30,median_ac30 = plot_one_snr(sm_corr_fa30,true_fa,'30 ac')
+
+cdf10,median10 = plot_one_snr(corpt_fa10,true_fa,'10')
+cdf_ec10,median_ec10 = plot_one_snr(bx_corr_fa10,true_fa,'10 ec')
+cdf_ac10,median_ac10 = plot_one_snr(sm_corr_fa10,true_fa,'10 ac')
+
+all_cdf = pd.concat([cdf,cdf_ec,cdf_ac,cdf100,cdf_ec100,cdf_ac100, cdf30, cdf_ec30,cdf_ac30,cdf10,cdf_ec10,cdf_ac10])
 all_cdf = all_cdf.rename(columns={0:'Abs percent error (%)'})
 plt.figure()
 sns.set(font_scale = 2)
-ax = sns.boxplot(data=all_cdf,hue='Tissue',x = 'SNR',y='Abs percent error (%)')
-ax.set_xticklabels(['','inf','100','30','10'])
-plt.ylim([-0.5,500])
+testPlot = sns.boxplot(data=all_cdf,hue='Tissue',x = 'SNR',y='Abs percent error (%)',palette="Set2")
+
+#m1 = all_cdf.groupby(['SNR', 'Tissue'])['Abs percent error (%)'].median().values
+m1 = np.concatenate([median,median_ec,median_ac,median100,median_ec100,median_ac100,median30,median_ec30,median_ac30,median10,median_ec10,median_ac10])
+mL1 = [str(np.round(s, 2)) for s in m1]
+ind = 0
+for tick in range(len(testPlot.get_xticklabels())):
+    testPlot.text(tick+.1, m1[ind+2]+1, mL1[ind+2],  horizontalalignment='center',  color='k', weight='semibold', fontsize=10)
+    testPlot.text(tick-.3, m1[ind+1]+1, mL1[ind+1],  horizontalalignment='center',  color='k', weight='semibold', fontsize=10)
+    testPlot.text(tick+.4, m1[ind]+1, mL1[ind], horizontalalignment='center', color='k', weight='semibold', fontsize=10)
+    ind += 3 
+
+testPlot.set_xticklabels(['','inf','','','100','','','30','','','10',''])
+plt.ylim([-10,175])
 plt.title('Effect of LR in different tissue at different SNR')
 plt.show()
 
-"""
-err_fa = bx_corr_fa - true_fa
-pe_fa = 100 * err_fa / true_fa
-ape_fa = np.abs(pe_fa)
-ape_fa[np.isnan(ape_fa)] = 0
-ape_fa_csf = np.where(seg == 0, ape_fa , math.nan) # for csf
-ape_fa_wm = np.where(seg == 1, ape_fa , math.nan)
-ape_fa_gm = np.where(seg == 2, ape_fa , math.nan)
-ape_fa_csf = ape_fa_csf.reshape(-1)
-ape_fa_csf = [x for x in ape_fa_csf if math.isnan(x) == False]
-ape_fa_gm = ape_fa_gm.reshape(-1)
-ape_fa_gm = [x for x in ape_fa_gm if math.isnan(x) == False]
-ape_fa_wm = ape_fa_wm.reshape(-1)
-ape_fa_wm = [x for x in ape_fa_wm if math.isnan(x) == False]
-df1 = pd.DataFrame(ape_fa_gm).assign(Tissue='GM')
-df2 = pd.DataFrame(ape_fa_wm).assign(Tissue='WM')
-df3 = pd.DataFrame(ape_fa_csf).assign(Tissue='CSF')
-cdf_ec = pd.concat([df1, df2, df3])
-cdf_ec = cdf_ec.assign(SNR = 'inf EC')
-
-err_fa100 = corpt_fa100 - true_fa
-pe_fa100 = 100 * err_fa100 / true_fa
-ape_fa100 = np.abs(pe_fa100)
-ape_fa_csf100 = np.where(seg == 0, ape_fa100 ,math.nan) # for csf
-ape_fa_wm100 = np.where(seg == 1, ape_fa100 ,math.nan)
-ape_fa_gm100 = np.where(seg == 2, ape_fa100 ,math.nan)
-ape_fa_csf100 = ape_fa_csf100.reshape(-1)
-ape_fa_gm100 = ape_fa_gm100.reshape(-1)
-ape_fa_wm100 = ape_fa_wm100.reshape(-1)
-ape_fa_csf100 = [x for x in ape_fa_csf100 if math.isnan(x) == False]
-ape_fa_gm100 = [x for x in ape_fa_gm100 if math.isnan(x) == False]
-ape_fa_wm100 = [x for x in ape_fa_wm100 if math.isnan(x) == False]
-df1 = pd.DataFrame(ape_fa_gm100).assign(Tissue='GM')
-df2 = pd.DataFrame(ape_fa_wm100).assign(Tissue='WM')
-df3 = pd.DataFrame(ape_fa_csf100).assign(Tissue='CSF')
-cdf100 = pd.concat([df1, df2, df3])
-cdf100 = cdf100.assign(SNR = '100')
-
-err_fa30 = corpt_fa30 - true_fa
-pe_fa30 = 100 * err_fa30 / true_fa
-ape_fa30 = np.abs(pe_fa30)
-ape_fa_csf30 = np.where(seg == 0, ape_fa30 ,math.nan) # for csf
-ape_fa_wm30 = np.where(seg == 1, ape_fa30 ,math.nan)
-ape_fa_gm30 = np.where(seg == 2, ape_fa30 ,math.nan)
-ape_fa_csf30 = ape_fa_csf30.reshape(-1)
-ape_fa_gm30 = ape_fa_gm30.reshape(-1)
-ape_fa_wm30 = ape_fa_wm30.reshape(-1)
-ape_fa_csf30 = [x for x in ape_fa_csf30 if math.isnan(x) == False]
-ape_fa_gm30 = [x for x in ape_fa_gm30 if math.isnan(x) == False]
-ape_fa_wm30 = [x for x in ape_fa_wm30 if math.isnan(x) == False]
-df1 = pd.DataFrame(ape_fa_gm30).assign(Tissue='GM')
-df2 = pd.DataFrame(ape_fa_wm30).assign(Tissue='WM')
-df3 = pd.DataFrame(ape_fa_csf30).assign(Tissue='CSF')
-cdf30 = pd.concat([df1, df2, df3])
-cdf30 = cdf30.assign(SNR = '30')
-
-err_fa10 = corpt_fa10 - true_fa
-pe_fa10 = 100 * err_fa10 / true_fa
-ape_fa10 = np.abs(pe_fa10)
-ape_fa_csf10 = np.where(seg == 0, ape_fa10 ,math.nan) # for csf
-ape_fa_wm10 = np.where(seg == 1, ape_fa10 ,math.nan)
-ape_fa_gm10 = np.where(seg == 2, ape_fa10 ,math.nan)
-ape_fa_csf10 = ape_fa_csf10.reshape(-1)
-ape_fa_gm10 = ape_fa_gm10.reshape(-1)
-ape_fa_wm10 = ape_fa_wm10.reshape(-1)
-ape_fa_csf10 = [x for x in ape_fa_csf10 if math.isnan(x) == False]
-ape_fa_gm10 = [x for x in ape_fa_gm10 if math.isnan(x) == False]
-ape_fa_wm10 = [x for x in ape_fa_wm10 if math.isnan(x) == False]
-df1 = pd.DataFrame(ape_fa_gm10).assign(Tissue='GM')
-df2 = pd.DataFrame(ape_fa_wm10).assign(Tissue='WM')
-df3 = pd.DataFrame(ape_fa_csf10).assign(Tissue='CSF')
-cdf10 = pd.concat([df1, df2, df3])
-cdf10 = cdf10.assign(SNR = '10')
-
-all_cdf = pd.concat([cdf,cdf_ec, cdf100, cdf30, cdf10])
-all_cdf = all_cdf.rename(columns={0:'Abs percent error (%)'})
-plt.figure()
-sns.set(font_scale = 2)
-ax = sns.boxplot(data=all_cdf,hue='Tissue',x = 'SNR',y='Abs percent error (%)')
-ax.set_xticklabels(['','inf','100','30','10'])
-plt.ylim([-0.5,500])
-plt.title('Effect of LR in different tissue at different SNR')
-plt.show()
-"""
