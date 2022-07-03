7794818912ecf5c27a69e6dc9af3e1ae13f8660e
praitayini <praitayini@gmail.com>
2022-07-03 11:17:35 -0500

    tract compute intrasession

--- src/get_session.py
+++ src/get_session.py
@@ -1,14 +1,52 @@
 import numpy as np
 import os
 import glob
-
+import matlab.engine
+eng = matlab.engine.start_matlab()
+eng.addpath('/home/local/VANDERBILT/kanakap/gradtensor/npy-matlab/')
 p = []
 for path in glob.glob('/nfs/masi/kanakap/projects/LR_tract/MASiVar_kids/*/*'):
     p.append(path)
 
 d = {}
 for i in range(len(p)):
-    a = [p[i]+'/tracto_op_1_est',p[i]+'/tracto_op_2_est']
+    a = [p[i]+'/tracto_op_1_est/connectivity_matrices/',p[i]+'/tracto_op_2_est/connectivity_matrices/']
     d[i] = a
+
+all_mod = {}
+all_gb = {}
+all_cpl = {}
+for i in d:
+    for f in os.listdir(d[i][0]):
+        if f.endswith('__tot_commit2_weights.npy'):
+            tot = d[i][0] + f 
+            if os.path.exists(tot):
+                mod = eng.compute_mod(tot)
+                gb = eng.compute_gb(tot)
+        if f.endswith('__len.npy'):
+            clen = d[i][0] + f
+            if os.path.exists(clen):
+                cpl = eng.compute_cpl(clen)
+    
+    for l in os.listdir(d[i][1]):
+        if l.endswith('__tot_commit2_weights.npy'):
+            tot1 = d[i][1]  + l
+            if os.path.exists(tot1):
+                mod1 = eng.compute_mod(tot1)
+                gb1 = eng.compute_gb(tot1)
+        if l.endswith('__len.npy'):
+            clen1 = d[i][1] + l
+            if os.path.exists(clen1):
+                cpl1 = eng.compute_cpl(clen1)
+
+    all_mod[i] = [mod, mod1]
+    all_gb[i] = [gb, gb1]
+    all_cpl[i] = [cpl, cpl1]
+eng.quit()
+print(all_mod)
+
+#~/MATLAB_2017b_install/bin/matlab
+        
+
 
 
